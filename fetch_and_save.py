4dfed38352c7d7bea4808675122165ef4268237b
mzuen <meitzuen@gmail.com>
2026-04-21 08:15:11 +0800

    Add punishment data and script

--- fetch_and_save.py
+++ fetch_and_save.py
@@ -5,6 +5,7 @@ import argparse
 from datetime import datetime
 from typing import Dict, Any
 
+
 def get_daily_index(date_str: str) -> Dict[str, Any]:
     index_url = (
         f"https://market.smallplum.xyz/api/strategy/daily/index?date_str={date_str}"
@@ -20,6 +21,17 @@ def get_daily_index(date_str: str) -> Dict[str, Any]:
 
 def get_daily_price(date_str: str) -> Dict[str, Any]:
     daily_url = f"https://market.smallplum.xyz/api/strategy/daily/stock_price?date_str={date_str}"
+    try:
+        response = requests.get(daily_url)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"抓取日線數據失敗: {e}")
+        return None
+
+
+def get_punish_stock(date_str: str) -> Dict[str, Any]:
+    daily_url = f"https://market.smallplum.xyz/api/strategy/daily/punishment?start_date={date_str}&end_date={date_str}"
     try:
         response = requests.get(daily_url)
         response.raise_for_status()
@@ -57,7 +69,11 @@ if __name__ == "__main__":
     daily_data = get_daily_index(date_str)
     if daily_data:
         save_to_file(daily_data, "daily_index", date_str)
-    
+
     daily_price_data = get_daily_price(date_str)
     if daily_price_data:
         save_to_file(daily_price_data, "daily_price", date_str)
+
+    daily_punish_data = get_punish_stock(date_str)
+    if daily_punish_data:
+        save_to_file(daily_punish_data, "daily_punish", date_str)
--- generate_manifest.py
+++ generate_manifest.py
@@ -1,27 +1,61 @@
 import os
 import json
 
-def generate_manifest():
-    data_dir = 'data'
-    manifest = {
-        'daily_index': [],
-        'daily_price': [],
-        'gap_jump': []
-    }
-    
-    if os.path.exists(os.path.join(data_dir, 'daily_index')):
-        manifest['daily_index'] = sorted([f.replace('.json', '') for f in os.listdir(os.path.join(data_dir, 'daily_index')) if f.endswith('.json')], reverse=True)
-        
-    if os.path.exists(os.path.join(data_dir, 'daily_price')):
-        manifest['daily_price'] = sorted([f.replace('.json', '') for f in os.listdir(os.path.join(data_dir, 'daily_price')) if f.endswith('.json')], reverse=True)
 
-    if os.path.exists(os.path.join(data_dir, 'gap_jump')):
-        manifest['gap_jump'] = sorted([f.replace('.json', '') for f in os.listdir(os.path.join(data_dir, 'gap_jump')) if f.endswith('.json')], reverse=True)
-        
-    with open(os.path.join(data_dir, 'manifest.json'), 'w', encoding='utf-8') as f:
+def generate_manifest():
+    data_dir = "data"
+    manifest = {
+        "daily_index": [],
+        "daily_price": [],
+        "gap_jump": [],
+        "daily_punish": [],
+    }
+
+    if os.path.exists(os.path.join(data_dir, "daily_index")):
+        manifest["daily_index"] = sorted(
+            [
+                f.replace(".json", "")
+                for f in os.listdir(os.path.join(data_dir, "daily_index"))
+                if f.endswith(".json")
+            ],
+            reverse=True,
+        )
+
+    if os.path.exists(os.path.join(data_dir, "daily_price")):
+        manifest["daily_price"] = sorted(
+            [
+                f.replace(".json", "")
+                for f in os.listdir(os.path.join(data_dir, "daily_price"))
+                if f.endswith(".json")
+            ],
+            reverse=True,
+        )
+
+    if os.path.exists(os.path.join(data_dir, "gap_jump")):
+        manifest["gap_jump"] = sorted(
+            [
+                f.replace(".json", "")
+                for f in os.listdir(os.path.join(data_dir, "gap_jump"))
+                if f.endswith(".json")
+            ],
+            reverse=True,
+        )
+
+    if os.path.exists(os.path.join(data_dir, "daily_punish")):
+        manifest["daily_punish"] = sorted(
+            [
+                f.replace(".json", "")
+                for f in os.listdir(os.path.join(data_dir, "daily_punish"))
+                if f.endswith(".json")
+            ],
+            reverse=True,
+        )
+
+    with open(os.path.join(data_dir, "manifest.json"), "w", encoding="utf-8") as f:
         json.dump(manifest, f, ensure_ascii=False, indent=4)
-    
+
     print("Manifest generated successfully.")
+
 
 if __name__ == "__main__":
     generate_manifest()
