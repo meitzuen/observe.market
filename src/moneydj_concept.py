e700238ba14b43214bf7a3d46a96aad6211b24f8
JamieM <Jamie.Mei@redcypress.com.tw>
2026-08-03 18:45:33 +0800

    Enhance the script

--- src/moneydj_concept.py
+++ src/moneydj_concept.py
@@ -12,6 +12,8 @@ pairs = re.findall(r'<option[^>]*value="?(EH\d+)"?[^>]*>([^<]+)</option>', html)
 concept_dict = {}
 
 for code, name in pairs:
-    concept_dict[code] = name.strip()
+    _name = name.strip()
+    _url = f'https://www.moneydj.com/z/zg/zge_{code}_1.djhtm'
+    concept_dict[_name] = _url
 
 print(concept_dict)
